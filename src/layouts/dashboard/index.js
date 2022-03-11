dce2ac0a5aa2cfbbc821f69be35ce4f28a8a9903
dinb95 <83233503+dinb95@users.noreply.github.com>
2022-03-11 13:43:33 +0200

    push for hadar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -38,14 +38,29 @@ import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
 const Dashboard = () => {
   const { sales, tasks } = reportsLineChartData;
   const [newUsers, setNewUsers] = useState(0);
+  const [allTrips, setAllTrips] = useState(0);
   const [newTrips, setNewTrips] = useState(0);
   const [newTripsForToday, setNewTripsForToday] = useState(0);
   const [newPopularChat, setNewPopularChat] = useState(0);
+  const [accordingDayChart, setAccordingDayChart] = useState({
+    labels: ["Sun", "M", "T", "W", "T", "F", "Sat"],
+    datasets: { label: "Routes", data: [0,0,0,0,0,0,0] }
+  });
+  const [accordingMonthChart, setAccordingMonthChart] = useState({
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July","Aug","Sept","Oct","Nov","Dec"],
+    datasets: { label: "Routes", data: [0,0,0,0,0,0,0,0,0,0,0,0] }
+  });
+  const [accordingHourChart, setAccordingHourChart] = useState({
+    labels: ["06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"],
+    datasets: { label: "Routes", data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] }
+  });
+
+
 
   useEffect(() => {
     getAllUser();
     getTrips();
-  })
+  }, [])
 
   const getAllUser=()=>{
     let api = "https://proj.ruppin.ac.il/bgroup54/test2/tar6/api/Users" //change to ruppin later.. also make a new publish
@@ -104,58 +119,52 @@ const getTrips=()=>{
     .then(
         (result) => { 
           console.log(result)
+          setAllTrips(result.length);
+
           let dt = new Date()
           let dtt = new Date()
           dt.setTime(dt.getTime() - 24*60*60*1000)
+
           let newToday = 0;
           let forToday = 0;
+          let routesDayCount = [0,0,0,0,0,0,0]
+          let routesMonthCount = [0,0,0,0,0,0,0,0,0,0,0,0]
+          let routesHoursCount = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
           result.forEach(route => {
+            let routeDt = new Date(route.date)
+
+            routesDayCount[routeDt.getDay()]++
+            routesMonthCount[routeDt.getMonth()]++
+            routesHoursCount[routeDt.getHours()-6]++
+
             if(route.addedDate != ""){
               let routeDt = new Date(route.addedDate)
               if(routeDt > dt)
                 newToday++;
             }
-            let routeDt = new Date(route.date)
+
             if((routeDt.getDate()==dtt.getDate())&&((routeDt.getMonth()==dtt.getMonth())&&(routeDt.getFullYear()==dtt.getFullYear()))){
               forToday++
             }
           })
+          const routeDayChart = {
+            labels: ["Sun", "M", "T", "W", "T", "F", "Sat"],
+            datasets: { label: "Routes", data: routesDayCount }
+          }
+          const routeMonthChart = {
+            labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July","Aug","Sept","Oct","Nov","Dec"],
+            datasets: { label: "Routes", data: routesMonthCount }
+          }
+          const routeHourChart = {
+            labels: ["06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"],
+            datasets: { label: "Routes", data: routesHoursCount }
+          }
+          setAccordingDayChart(routeDayChart)
+          setAccordingMonthChart(routeMonthChart)
+          setAccordingHourChart(routeHourChart);
           setNewTrips(newToday);
           setNewTripsForToday(forToday);
-          }
-        )
-    .catch(function(error) {
-      console.log('There has been a problem with your fetch operation: ' + error.message);
-        throw error;
-      });
-}
-/////////////////////////////////////////////////////////////////////////////////////////////
-const plannedTripsForToday=()=>{
-  let api = "https://proj.ruppin.ac.il/bgroup54/test2/tar6/api/RouteData" //change to ruppin later.. also make a new publish
-  fetch(api, {
-    method: "GET",
-    headers: new Headers({
-        'Content-type': 'application/json; charset=UTF-8',
-        'Accept': 'application/json; charset=UTF-8'
-      })
-    })
-    .then(res => {
-        return res.json()
-    })
-    .then(
-        (result) => { 
-          console.log(result)
-          let dt = new Date()
-          dt.setTime(dt.getTime() - 24*60*60*1000)
-          let newToday = 0;
-          result.Users.forEach(user => {
-            // if(user.Dt != ""){
-            //   let userDt = new Date(user.Dt)
-            //   if(userDt > dt)
-            //     newToday++;
-            // }
-          })
-          setNewTripsForToday(newTripsForToday);
           }
         )
     .catch(function(error) {
@@ -237,12 +246,12 @@ const mostPopularChat=()=>{
               <ComplexStatisticsCard
                 color="dark"
                 icon="map"
-                title="planned trips for today"
-                count={newTripsForToday}
+                title="Total Trips In Timely"
+                count={allTrips}
                 percentage={{
                   color: "success",
-                  amount: "+1%",
-                  label: "than yesterday",
+                  amount: newTripsForToday,
+                  label: "Trips planned for today",
                 }}
               />
             </MDBox>
@@ -270,13 +279,13 @@ const mostPopularChat=()=>{
               {/* change here for charts */}
               {/* 
                 {labels: ["M", "T", "W", "T", "F", "S", "S"],
-                datasets: { label: "Sales", data: [50, 20, 10, 22, 50, 10, 40] },}
+                datasets: { label: "Sales", data: [50, 20, 10, 22, 50, 10, 40] }}
              */}
                 <ReportsBarChart
                   color="info"
                   title="Trips are planned by day"
                   date="need to complite"
-                  chart={reportsBarChartData}
+                  chart={accordingDayChart}
                 />
               </MDBox>
             </Grid>
@@ -286,7 +295,7 @@ const mostPopularChat=()=>{
                   color="success"
                   title="Trips are planned by month"
                   date="updated 4 min ago"
-                  chart={sales}
+                  chart={accordingMonthChart}
                 />
               </MDBox>
             </Grid>
@@ -294,9 +303,9 @@ const mostPopularChat=()=>{
               <MDBox mb={3}>
                 <ReportsLineChart
                   color="dark"
-                  title="Trips are planned by houre"
+                  title="Trips are planned by hours"
                   date="just updated"
-                  chart={tasks}
+                  chart={accordingHourChart}
                 />
               </MDBox>
             </Grid>
